96a4280bd8329eabeb1cdc23d47db0a74574bb14
linhua sun <linhuasun@linhuadeMacBook-Pro-2.local>
2026-01-31 00:51:55 +0800

    chore: remove debug scripts before build check

--- src/scripts/check_db.ts
+++
@@ -1,8 +0,0 @@
-
-import { PrismaClient } from '@prisma/client';
-const prisma = new PrismaClient();
-async function main() {
-    const count = await prisma.analysisReport.count();
-    console.log('Total reports:', count);
-}
-main();
